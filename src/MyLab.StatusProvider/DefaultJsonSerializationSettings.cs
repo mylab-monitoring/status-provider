450cb7b6e0eb657cf8499c0860c8a48152bb5604
Andrey O <ozzy@triasoft.com>
2020-10-29 18:59:13 +0300

    No $type in JSON by default

---
+++ src/MyLab.StatusProvider/DefaultJsonSerializationSettings.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace MyLab.StatusProvider
+{
+    static class DefaultJsonSerializationSettings
+    {
+        public static JsonSerializerSettings Create()
+        {
+            return new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.None
+            };
+        }
+    }
+}
